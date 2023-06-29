8c7d4fe8dd1f4ad9c7877f78505d526898464c6f
Temitope Balogun <tsbalogun97@yahoo.com>
2023-06-28 23:04:39 -0500

    updated with bulleye and score

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ const darts = () => {
   dart.classList.toggle('darthidden')
 }
 
-
+let num = 0
+let num2 = 0
 const scoreLeft = () => {
   let score = document.querySelector('.leftScore')
-  let numberA = numberA +10
-  score.innerHTML = numberA
+  num += 10
+  score.innerHTML = `Score: ${num}`
 
 }
 
+const scoreRight = () => {
+  let score = document.querySelector('.rightScore')
+  num2 += 10
+  score.innerHTML = `Score: ${num2}`
+}
+
+const restart = () => {
+  let score = document.querySelector('.rightScore') 
+  let point = document.querySelector('.leftScore')
+  score.innerHTML = "Score"
+  point.innerHTML = "Score"
+  num = 0 
+  num2 = 0
+}
