34c6aadc807e492323bb8f0f6ed1fd3e348ae1aa
Temitope Balogun <tsbalogun97@yahoo.com>
2023-06-28 22:28:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const darts = () => {
+  let dart = document.querySelector('.dart')
+  dart.classList.toggle('darthidden')
+}
+
+
+const scoreLeft = () => {
+  let score = document.querySelector('.leftScore')
+  let numberA = numberA +10
+  score.innerHTML = numberA
+
+}
+
