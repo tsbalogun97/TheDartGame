9526fb21e9513054272715cd2a5cf69cc612c067
Temitope Balogun <tsbalogun97@yahoo.com>
2023-06-30 12:36:11 -0500

    updated with boardScore, dartPosition, readme

--- app.js
+++ app.js
@@ -3,32 +3,45 @@ const darts = () => {
   let dart = document.querySelector('.dart')
   dart.classList.toggle('darthidden')
 }
+// storing number where dart hit
+let dartPosition;
+// object that stores the point for hit location for playOne 
+let boardScore = { 
+  1:0, 19:0, 6:0, 24:0, 2:20, 3:30, 4:30,
+  5:10, 7:20, 8:50, 9:100, 10:100,
+  11:50, 12:20, 13:20, 14:50, 15:70,
+  16:70, 17:50, 18:10, 20:10, 21:20,
+  22:20, 23:10 
+}
 
 // creating score count for both players
 let num = 0
 let num2 = 0
 const scoreLeft = () => {
+  moveDart()
   let score = document.querySelector('.leftScore')
-  num += 10
-  score.innerHTML = `Score: ${num}`
+  num += boardScore[dartPosition]
+  score.innerHTML = `Score: ${num}, dartPosition ${dartPosition}`
 
 }
 
 const scoreRight = () => {
-  let score = document.querySelector('.rightScore')
-  num2 += 10
-  score.innerHTML = `Score: ${num2}`
+  score = document.querySelector('.rightScore')
+  num2 += boardScore[dartPosition]
+  score.innerHTML = `Score: ${num2}, dartPosition ${dartPosition}`
+  
 }
 
 
 //  resetting the game 
 const restart = () => {
-  let score = document.querySelector('.rightScore') 
-  let point = document.querySelector('.leftScore')
-  score.innerHTML = "Score"
-  point.innerHTML = "Score"
-  num = 0 
-  num2 = 0
+  location.reload()
+  // let score = document.querySelector('.rightScore') 
+  // let point = document.querySelector('.leftScore')
+  // score.innerHTML = "Score"
+  // point.innerHTML = "Score"
+  // num = 0 
+  // num2 = 0
 }
 
 // creating function to move the dart
@@ -36,29 +49,52 @@ const restart = () => {
 const moveDart = () => {
   let boxes = document.querySelectorAll('.box')
   console.log(boxes)
-  let currentBox = boxes[randomSpots(0,23)]
+  dartPosition = randomSpots(1, 24)
+  let currentBox = boxes[dartPosition -1]
   let dart = document.createElement('img')
   dart.className = "dart"
   // let dart = document.querySelector('.dart').cloneNode()
   currentBox.append(dart)
-
 }
+
 // random hitspots
 const randomSpots =(min,max) => {
   return Math.floor(min + Math.random()* (max-min))
   
 }
-
-
-
-
 // creating countdowm timer
+
 // creating win state and lose state
-// add a prompt that say "gamer over!""
+
+// call the function to start the game/check the game status
+const startGame = () => {
+  gameStatus()
+  if(score > 500) {
+    gameOver()
+  }else{
+    console.log(`${playerTwo} playerTwo turn to throw his dart`)
+    }
+  }
+
+
+// game over function
 const gameOver = () => {
   alert('GAMEOVER')
   location.reload() 
+// to reload the entire browser to restart the game
+
 }
+
+// create a function that checks the game is still ongoing.
+const gameStatus = () =>{
+  if(score > 500) {
+    console.log(`%c GAME OVER! ${playerOne} wins`)
+    gameOver()
+  }else{
+    console.log(`%c BUMMER! ${playerTwo} wins`) 
+  }
+}
+
 
 // add key frames that shows messages like " you miss your shot"
 
---
+++ readme.md
@@ -0,0 +1,6 @@
+This bulleye game comprises of two players throwing darts randomly. 
+Each spots on the bulleye field will have different points. The closest you are to the center, the more points will be generated. 
+There will be a set timer for this game that will determine who won the game.
+There is a reset button to reset the game. 
+There will be an alert that states who won and also flagging game over.
+To shoot your dart, you have to click on the players names.
